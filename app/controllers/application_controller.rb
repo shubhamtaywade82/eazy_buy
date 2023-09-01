aa2de69938b44bf6f9cb6fe5e3e9befad3470454
Shubham Taywdae <shubhamtaywdae82@gmail.com>
2023-09-02 01:01:43 +0530

    Updated SignIn/Login View using forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # check_authorization unless: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
 
   protected
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  role                   :string
 #  username               :string           not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
@@ -24,4 +25,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Roles
+  enum role: { customer: "customer", admin: "admin", seller: "seller" }
 end
